6c49b5fbffd2a4a2a11c5b6ed491b1f315a55da1
jayrodgalaus <globalInnuendo@gmail.com>
2025-07-01 01:13:16 +0800

    Update ai.js

--- public/assets/js/ai.js
+++ public/assets/js/ai.js
@@ -32,13 +32,13 @@ function mistralCheckDraft(element){
         message = `${mistral_improvements[getRandomIndex(mistral_improvements)]}
             <hr>
             <div class="d-flex align-items-center justify-content-center">
-                <div>
-                    <button type="button" class="btn btn-primary mx-1 mistral-improve-draft" textarea="${textarea}" type="${type}">Shorten</button>
-                    <button type="button" class="btn btn-primary mx-1 mistral-improve-draft" textarea="${textarea}" type="${type}">Lengthen</button>
+                <div class="d-flex flex-column align-items-center justify-content-center">
+                    <button type="button" class="btn btn-primary m-1 w-100 mistral-improve-draft" textarea="${textarea}" type="${type}">Shorten</button>
+                    <button type="button" class="btn btn-primary m-1 w-100 mistral-improve-draft" textarea="${textarea}" type="${type}">Lengthen</button>
                 </div>
-                <div>
-                    <button type="button" class="btn btn-primary mx-1 mistral-improve-draft" textarea="${textarea}" type="${type}">Clean up</button>
-                    <button type="button" class="btn btn-primary mx-1 mistral-improve-draft" textarea="${textarea}" type="${type}">Paraphrase</button>
+                <div class="d-flex flex-column align-items-center justify-content-center">
+                    <button type="button" class="btn btn-primary m-1 w-100 mistral-improve-draft" textarea="${textarea}" type="${type}">Clean up</button>
+                    <button type="button" class="btn btn-primary m-1 w-100 mistral-improve-draft" textarea="${textarea}" type="${type}">Paraphrase</button>
                 </div>
             </div>
         `;
